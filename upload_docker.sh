f9bc27d1f145e8a74700700f95f99c33892f7a41
NatwestProj <96770848+NatwestProj@users.noreply.github.com>
2022-02-02 23:01:12 +0530

    up

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=NatwestProj/mlpred
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+
+docker login -u sunnynatwest
+docker tag mlpred $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
